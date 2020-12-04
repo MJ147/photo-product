393bccbed84d74fc199c755eb4f68f989c2116d6
Marek Jaczewski <jaczewski147@gmail.com>
2020-12-04 21:46:08 +0100

    formatter configure

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.less']
+	selector: 'app-main',
+	templateUrl: './main.component.html',
+	styleUrls: ['./main.component.less'],
 })
 export class MainComponent implements OnInit {
+	images: Image[] = [];
+	isBorder: boolean = true;
 
-  constructor() { }
+	constructor(private sanitizer: DomSanitizer) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	addImage(files: File[]): void {
+		const unsafeImageUrl = URL.createObjectURL(files[0]);
+		const image: Image = {
+			img: this.sanitizer.bypassSecurityTrustUrl(unsafeImageUrl),
+			scale: 100,
+			numberOfCopies: 1,
+			margin: 0,
+		};
+		this.images.push(image);
+	}
 }
