8cb3628e7e9251a36558d29f410e99e19e1a2a36
Marek Jaczewski <jaczewski147@gmail.com>
2020-12-06 18:48:26 +0100

    load and show images in viewport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, MainComponent],
+	imports: [BrowserModule, AppRoutingModule, MaterialModule, BrowserAnimationsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import * as p5 from 'p5';
+
+interface Image {
+	img: SafeUrl;
+	scale: number;
+	numberOfCopies: number;
+	margin: number;
+}
 
 @Component({
 	selector: 'app-main',
@@ -8,19 +17,45 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 	images: Image[] = [];
 	isBorder: boolean = true;
+	p5;
+	canvas;
+	x = 0;
+	img = [];
 
-	constructor(private sanitizer: DomSanitizer) {}
+	constructor(public sanitizer: DomSanitizer, private _elementRef: ElementRef) {}
 
 	ngOnInit(): void {}
 
-	addImage(files: File[]): void {
-		const unsafeImageUrl = URL.createObjectURL(files[0]);
+	addImage(event): void {
+		const unsafeImageUrl = URL.createObjectURL(event.target.files[0]);
 		const image: Image = {
-			img: this.sanitizer.bypassSecurityTrustUrl(unsafeImageUrl),
+			img: unsafeImageUrl,
 			scale: 100,
 			numberOfCopies: 1,
 			margin: 0,
 		};
+
 		this.images.push(image);
+	}
+
+	createImage() {
+		const viewport = this._elementRef.nativeElement.querySelector('viewport');
+		const p = (p) => {
+			p.setup = () => {
+				p.createCanvas(700, 410);
+				p.background(255);
+				this.images.forEach((image) => {
+					this.img.push(p.loadImage(image.img));
+				});
+			};
+			p.draw = () => {
+				this.img.forEach((image, index) => {
+					const scale = 200 / image.height;
+					p.image(image, index * 200, 0, image.width, image.height);
+					image.resize(300, 0);
+				});
+			};
+		};
+		this.canvas = new p5(p, viewport);
 	}
 }
