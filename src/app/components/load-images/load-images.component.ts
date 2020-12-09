3380e2d544102a86abd70df6253372748667eec3
Marek Jaczewski <jaczewski147@gmail.com>
2020-12-09 22:32:33 +0100

    copy picture

--- src/app/components/load-images/load-images.component.ts
+++ src/app/components/load-images/load-images.component.ts
@@ -4,6 +4,7 @@ import { ImageWrapper } from './../../models/image-wrapper';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'app-load-images',
@@ -12,6 +13,10 @@ import { Router } from '@angular/router';
 })
 export class LoadImagesComponent implements OnInit {
 	images: ImageWrapper[];
+	selectedImage: ImageWrapper = null;
+
+	columns: FormControl = new FormControl(1);
+	rows: FormControl = new FormControl(1);
 
 	constructor(private _imageService: ImageService, private _router: Router, public sanitizer: DomSanitizer) {}
 
@@ -27,5 +32,9 @@ export class LoadImagesComponent implements OnInit {
 
 	createImage(): void {
 		this._router.navigate(['create']);
+	}
+
+	selectImage(image: ImageWrapper): void {
+		this.selectedImage = image;
 	}
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
 	imports: [CommonModule],
-	exports: [MatButtonModule, MatIconModule],
+	exports: [MatInputModule, MatButtonModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule],
 })
 export class MaterialModule {}
